94a5a05c4819f23f8bd84932c57d392e6025aabd
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2025-11-30 02:30:59 -0500

    feat: :sparkles: add comprehensive map examples in Go, including inline initialization, make, deletion, and comparison

--- 3_Variables/main.go
+++ 3_Variables/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	var cadena = "Inicial" // Tipado implicito
 	fmt.Println(cadena)
+	fmt.Printf("Tipo de cadena: %T\n", cadena)
 
 	var cadena2 string
 	fmt.Println(cadena2) // Valor por defecto: ""
@@ -26,10 +27,16 @@ func main() {
 	fruta := "manzana"
 	// Lo mismo que: var fruta string = "manzana"
 	fmt.Println(fruta)
+	fmt.Printf("Tipo de fruta: %T\n", fruta)
+	fmt.Println("-----------------------------------------------")
 
 	x := 10         // int
 	pi := 3.14      // float64
 	ok := true      // bool
 	nombre := "Ana" // string
 	fmt.Println(x, pi, ok, nombre)
+	fmt.Printf("Tipo de x: %T\n", x)
+	fmt.Printf("Tipo de pi: %T\n", pi)
+	fmt.Printf("Tipo de ok: %T\n", ok)
+	fmt.Printf("Tipo de nombre: %T\n", nombre)
 }
--- 4_Valores/main.go
+++ 4_Valores/main.go
@@ -8,12 +8,18 @@ func main() {
 	numero2 := 20
 	fmt.Println(numero1 + numero2)
 
+	fmt.Printf("Tipo de numero2: %T\n", numero2)
+
 	numero3 := 20.5                     // float64
 	resultado := numero1 + int(numero3) // 20.5 se convierte a 20 no 21
 	fmt.Println(resultado)
+
+	fmt.Printf("Tipo de numero3: %T\n", numero3)
 
 	var nombre string = "Oscar"
 	apellido := "Giraldo"
 	nombreCompleto := nombre + " " + apellido
 	fmt.Println(nombreCompleto)
+
+	fmt.Printf("Tipo de apellido: %T\n", apellido)
 }
