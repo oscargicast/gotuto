8c166e46376799173778566f625644b680190856
Oscar Giraldo Castillo <oscar@getcometa.com>
2025-12-28 00:26:49 -0500

    chore: complete mock test

--- 22_1_Mocking/main_test.go
+++ 22_1_Mocking/main_test.go
@@ -28,6 +28,51 @@ func TestGetFullTimeEmployeeById(t *testing.T) {
 					}, nil
 				}
 			},
+			expectedEmployee: FullTimeEmployee{
+				Person: Person{
+					DNI:  "1",
+					Name: "Oscar Giraldo",
+					Age:  35,
+				},
+				Employee: Employee{
+					ID:       1,
+					Position: "CEO",
+				},
+			},
 		},
+	}
+
+	originalGetEmployeeByID := GetEmployeeByID
+	originalGetPersonByDNI := GetPersonByDNI
+
+	for _, test := range table {
+		test.mockFunc()
+		ft, err := GetFullTimeEmployeeByID(test.id, test.dni)
+		if err != nil {
+			t.Errorf("Error when getting Fulltime Employee %v", err)
+		}
+
+		if ft.DNI != test.expectedEmployee.DNI {
+			t.Errorf("Error, got %s expected %s", ft.DNI, test.expectedEmployee.DNI)
+		}
+
+		if ft.Name != test.expectedEmployee.Name {
+			t.Errorf("Error, got %s expected %s", ft.Name, test.expectedEmployee.Name)
+		}
+
+		if ft.Age != test.expectedEmployee.Age {
+			t.Errorf("Error, got %d expected %d", ft.Age, test.expectedEmployee.Age)
+		}
+
+		if ft.ID != test.expectedEmployee.ID {
+			t.Errorf("Error, got %d expected %d", ft.ID, test.expectedEmployee.ID)
+		}
+
+		if ft.Position != test.expectedEmployee.Position {
+			t.Errorf("Error, got %s expected %s", ft.Position, test.expectedEmployee.Position)
+		}
+
+		GetEmployeeByID = originalGetEmployeeByID
+		GetPersonByDNI = originalGetPersonByDNI
 	}
 }
